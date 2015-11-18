fbc27cf7229afffb1e7ea1142c32ff8ce9edf08a
Felix Hammerl <felix.hammerl@gmail.com>
2015-11-18 03:22:32 +0100

    It was late

--- test/integration/browserbox-test.js
+++ test/integration/browserbox-test.js
@@ -204,10 +204,6 @@
                 imap.close();
             });
 
-            it('foofooo', function(done) {
-                imap.listNamespaces().then(done);
-            });
-
             describe('#listMailboxes', function() {
                 it('should succeed', function(done) {
                     imap.listMailboxes(function(err, mailboxes) {
