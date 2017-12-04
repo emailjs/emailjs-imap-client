df107cbd580b4ead239bca4a7b7547d7561e8503
Felix Hammerl <fhammerl@thoughtworks.com>
2017-12-04 15:13:14 +0100

    Exlude integration and worker files from dist

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 npm run build-worker
 rm -rf $PWD/dist
-babel src --out-dir dist --ignore '**/*-unit.js' --source-maps inline
+babel src --out-dir dist --ignore '**/*-unit.js','**/*-integration.js','**/*-worker.js' --source-maps inline
 git reset
 git add $PWD/dist
 git commit -m 'Updating dist files' -n
