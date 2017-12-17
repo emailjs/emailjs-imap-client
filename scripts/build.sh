dea49215554cdf098eacd893c74d4bd16303028d
Felix Hammerl <fhammerl@thoughtworks.com>
2017-12-17 18:36:44 +0100

    do not automatically trigger git commits after build steps

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,3 @@
 npm run build-worker
 rm -rf $PWD/dist
 babel src --out-dir dist --ignore '**/*-unit.js','**/*-integration.js','**/*-worker.js' --source-maps inline
-git reset
-git add $PWD/dist
-git commit -m 'Updating dist files' -n
--- scripts/worker.sh
+++ scripts/worker.sh
@@ -3,6 +3,3 @@
 rm $PWD/res/compression.worker.blob
 webpack -p
 mv $PWD/res/compression.worker.js $PWD/res/compression.worker.blob
-git reset
-git add $PWD/res/compression.worker.blob
-git commit -m 'Updating worker blob' -n
